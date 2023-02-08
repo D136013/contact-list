31ed6418720a92b24ee8acccd7a2c31f09217434
Diego <diegosk.fg@gmail.com>
2023-02-07 21:37:11 -0500

    creada rama project-2 con objetos

--- contactos.js
+++ contactos.js
@@ -1,14 +1,32 @@
-let listaContactos = ["Diego Florez", "Eskarleth Florez", "Alana Florez"];
+let listaContactos = [];
 
-function agregarContacto(contacto){
-    listaContactos.unshift(contacto);
+function agregarContacto(id, nombres, apellidos, telefono, ciudad, direccion){
+    let contacto = {
+        id: id,
+        nombres: nombres,
+        apellidos: apellidos,
+        telefono: telefono,
+        ubicaciones: {
+            ciudad: ciudad,
+            direccion: direccion
+        } 
+    }
+    listaContactos.push(contacto);
 }
 
-function borrarContacto(contacto){
-    listaContactos = listaContactos.filter((persona) => persona !== contacto);
+function borrarContacto(id){
+    listaContactos = listaContactos.filter((persona) => persona.id !== id);
 }
 
 function mostrarContacto(){
-    listaContactos.sort();
     listaContactos.forEach((contacto) => console.log(contacto));
 }
+
+
+agregarContacto(1, "diego jose", "Florez Gelves", "963", "zipaquira", "villa maria");
+agregarContacto(2, "carlos julian", "lara mora", "258", "zipaquira", "villa maria");
+console.log(listaContactos)
+borrarContacto()
+borrarContacto()
+
+console.log(listaContactos)
